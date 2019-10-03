53a3779d7434cb4923c90c6f1ee45c3b9a59c1c9
Moxley Stratton <moxley.stratton@gmail.com>
2019-10-03 09:31:54 -0700

    Use main context for entry point

--- lib/open_api_parse.ex
+++ lib/open_api_parse.ex
@@ -1,18 +1,3 @@
 defmodule OpenApiParse do
-  @moduledoc """
-  Documentation for OpenApiParse.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> OpenApiParse.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  defdelegate parse_file(file), to: OpenApiParse.Parse
 end
